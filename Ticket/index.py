ee42e36a91a50f627668449f323304cf0c5df88b
RodrigoCCG <rodrigo_galletto@edu.unifil.br>
2026-02-24 16:54:44 -0300

    Removed Academic System Dependencies

--- Ticket/index.py
+++ Ticket/index.py
@@ -2,8 +2,10 @@ from flask import Flask, redirect, url_for, render_template, request, session
 import json
 import requests
 import time
-from werkzeug.utils import secure_filename
 import os
+import random
+from dotenv import load_dotenv
+load_dotenv()
 
 movi_url = 'https://unifil.movidesk.com/'
 movi_api_url = movi_url+'public/v1/'
@@ -47,6 +49,7 @@ def create_new_user(cpf,name,email):
         'password' : str(cpf),
     }
     print(movi_person_data)
+    movi_token = os.environ.get("MOVI_TOKEN")
     user = requests.post(movi_api_url+'persons?'+movi_token, json = movi_person_data)
     time.sleep(0.5)
     print("created user")
@@ -72,6 +75,7 @@ def create_tutor_user(cpf,name,email,course_name):
         'password' : str(cpf),
         'teams': ["Curso - "+str(course_name)]
     }
+    movi_token = os.environ.get("MOVI_TOKEN")
     course = requests.post(movi_api_url+'persons?'+movi_token, json = movi_person_data)
     print("create Tutor" + str(course))
     time.sleep(0.5)
@@ -91,6 +95,7 @@ def create_course_user(id_curso,name):
         'password' : str(id_curso)+"12345678",
         'teams': ["Curso - "+str(name)]
     }
+    movi_token = os.environ.get("MOVI_TOKEN")
     course = requests.post(movi_api_url+'persons?'+movi_token, json = movi_person_data)
     time.sleep(0.5)
     print("Discipline" + str(course))
@@ -109,6 +114,7 @@ def hello_world():
 
 @app.route("/sendToMovi", methods = ['POST'])
 def send_to_movi():
+    movi_token = os.environ.get("MOVI_TOKEN")
     #Check if Form is filled or return error
     print("Check if Form is filled or return error")
     if (str(request.form["text_input"]) == "") or (str(request.form["category_type"]) == "") or (str(request.form["subject_type"]) == ""):
@@ -125,7 +131,11 @@ def send_to_movi():
     #alimentar Webhook
     print("alimentar Webhook")
     if str(request.form["custom_canvas_course_id"]) != "$Canvas.course.id":
-        enrollment_data = requests.get("https://unifil.instructure.com/api/v1/courses/"+str(request.form["custom_canvas_course_id"])+"/enrollments",headers={'Authorization':"Bearer 9257~YJvuvONmkSWW6oErTiT9kIURR9RKZqFtKDon0pwOhJ6LhR6B3GsbK8jTYPjoRIzt"} ,verify="./static/certs.pem")
+        canvas_token = os.environ.get("CANVAS_TOKEN")
+        print(canvas_token)
+        enrollment_data = requests.get("https://unifil.instructure.com/api/v1/courses/"+str(request.form["custom_canvas_course_id"])+"/enrollments?access_token="+canvas_token,headers={'Authorization':"Bearer "+canvas_token} ,verify="./static/certs.pem")
+        course_data = requests.get("https://unifil.instructure.com/api/v1/courses/"+str(request.form["custom_canvas_course_id"])+"?access_token="+canvas_token,headers={'Authorization':"Bearer "+canvas_token} ,verify="./static/certs.pem")
+        print(json.loads(course_data.content))
         if (len(json.loads(enrollment_data.content)) > 0):
             print(json.loads(enrollment_data.content))
             enrollment_data = json.loads(enrollment_data.content)[0]
@@ -133,20 +143,55 @@ def send_to_movi():
             enrollment_data = {"sis_section_id" : "None"}
     else:
         enrollment_data = {"sis_section_id" : "None"}
-
+    print(enrollment_data)
+    """
     #Get student Data from Webook
-    print("Get student Data from Webook")
-    bearer = requests.post("https://webhook.unifil.br/login",headers={"Content-Type": "application/json"},data=json.dumps({"username":"tamadeu","email":"tamadeu@unifil.br","password":"1b396ec5-5b2a-4bcf-88d6-c63800b15408"}) ,verify="./static/certs.pem")
-    time.sleep(0.5)
-    bearer = json.loads(bearer.content)["token"]
-    student_data = requests.get("https://webhook.unifil.br/aluno",headers={'Authorization':"Bearer "+bearer,"Content-Type": "application/json"},data=json.dumps({"sis_section_id" : str(enrollment_data["sis_section_id"]), "cpf" : str(request.form["custom_user_login"]) }) ,verify="./static/certs.pem")
-    if(json.loads(student_data.content)["aluno"] == []): return render_template("index.html", requestData = request.form, error ="O seu usuário não foi encontrado no sistema de Ticket, entre em contato com um coordenador/suporte. (Codigo: Erro_de_Webhook:STUDENT_NOT_FOUND)")
-    student_data = json.loads(student_data.content)["aluno"][0]
-    print(student_data)
-    if student_data["nome_aluno"] == None:
-        return render_template("index.html", requestData = request.form, error ="Erro: Aluno não cadastrado")
-    if len(student_data["nome_curso"]) > 64:
+        print("Get student Data from Webook")
+        bearer = requests.post("https://webhook.unifil.br/login",headers={"Content-Type": "application/json"},data=json.dumps({"username":"tamadeu","email":"tamadeu@unifil.br","password":"1b396ec5-5b2a-4bcf-88d6-c63800b15408"}) ,verify="./static/certs.pem")
+        time.sleep(0.5)
+        bearer = json.loads(bearer.content)["token"]
+        student_data = requests.get("https://webhook.unifil.br/aluno",headers={'Authorization':"Bearer "+bearer,"Content-Type": "application/json"},data=json.dumps({"sis_section_id" : str(enrollment_data["sis_section_id"]), "cpf" : str(request.form["custom_user_login"]) }) ,verify="./static/certs.pem")
+        if(json.loads(student_data.content)["aluno"] == []): return render_template("index.html", requestData = request.form, error ="O seu usuário não foi encontrado no sistema de Ticket, entre em contato com um coordenador/suporte. (Codigo: Erro_de_Webhook:STUDENT_NOT_FOUND)")
+        student_data = json.loads(student_data.content)["aluno"][0]
+        print(student_data)
+        if student_data["nome_aluno"] == None:
+            return render_template("index.html", requestData = request.form, error ="Erro: Aluno não cadastrado")
+        if len(student_data["nome_curso"]) > 64:
         student_data["nome_curso"] = student_data["nome_curso"][:60]
+    """
+    ticket_context = ""
+    if enrollment_data["sis_section_id"] != "None":
+        course_data = json.loads(course_data.content)
+        course_code = course_data["course_code"][:3]
+        with open('course_tag.json') as f:
+            course_tag = json.load(f)
+        with open('tutor_data.json') as f:
+            tutor_data = json.load(f)
+        if course_code in course_tag:
+            tutor_id = random.sample(course_tag[course_code]["tutors"],k=1)[0]
+            if(not tutor_data[tutor_id]["active"]):
+                for t in course_tag[course_code]["tutors"]:
+                    tutor_id = t
+                    if tutor_data[tutor_id]["active"]:
+                        break
+            student_data = {
+                "cpf_tutor":tutor_id,
+                "nome_tutor":tutor_data[tutor_id]["nome"],
+                "nome_curso":course_tag[course_code]["name"]
+            }
+        else:
+            ticket_context = "<p style=\"box-sizing: border-box; margin: 0\">Código da Disciplina:" +course_data["course_code"]+"</p><p style=\"box-sizing: border-box; margin: 0\">Nome da Disciplina"+course_data["name"]+"</p>"
+            student_data = {
+                "cpf_tutor":None,
+                "nome_tutor":"Supervisão Tutoria EaD",
+                "nome_curso":"Redirecionar"
+            }
+    else:
+        student_data = {
+            "cpf_tutor":None,
+            "nome_tutor":"Supervisão Tutoria EaD",
+            "nome_curso":"Redirecionar"
+        }
 
     #Atrela o Tutor ao Ticket
     owner_name = ""
@@ -216,7 +261,7 @@ def send_to_movi():
     "actions" : [
         {
         "type" : 2,
-        "description" : str(request.form["text_input"])
+        "description" : (ticket_context + str(request.form["text_input"]))
         }
     ],
     "customFieldValues" : [
@@ -230,7 +275,7 @@ def send_to_movi():
         "customFieldId" : 137235,
         "customFieldRuleId" : 69373,
         "line" : 1,
-        "value" : str(student_data["nome_polo"])
+        "value" : str("Indisponivel")
         },
     ]
     }
